fd8848b3bb45849022c10e6bae192626ab1b93d5
Craner <iayfnp@gmail.com>
2023-02-08 07:36:05 -0800

    small tweaks

--- Nintendo Tumbler Puzzle Reinforcement Learning/main.py
+++ Nintendo Tumbler Puzzle Reinforcement Learning/main.py
@@ -6,10 +6,10 @@ import random
 from Tumbler import Tumbler
 
 # https://www.geeksforgeeks.org/implementing-neural-networks-using-tensorflow/
+# lists are not type sensitive. numpy arrays are type sensitive
 # print(model.predict(np.array([state.scrub(table[4])])))
 # print(model.predict(state.scrub_all()))
 # print(model.predict(X[0:1]))
-# lists are not type sensitive. numpy arrays are type sensitive
 
 def Synthesize():
     print("Data Synthesis")
@@ -56,13 +56,14 @@ def Synthesize():
 def Import(fstream):
     print("Importing Data")
 
-    if fstream == 'data.json':
-        global epoch, model
-        epoch = int(open('text.txt', "r").read())
+    global epoch, model
+    epoch = 0
+    if fstream == 'buffer.json':
+        epoch = int(open('progress.txt', 'r').read())
         # loads the weights. automatically compiles the model. 
         model = keras.models.load_model('model.h5')
     
-    data = json.load(open(fstream, "r"))
+    data = json.load(open(fstream, 'r'))
     for i, (key, val) in enumerate(data.items()):
         X[i] = np.array(json.loads(key))
         Y[i] = val
@@ -70,16 +71,16 @@ def Import(fstream):
 def Export(fstream):
     print("Exporting Data")
     
-    if fstream == "":
-        f = open('text.txt', "w")
-        f.write(str(epoch))
-        f.close()
+    if fstream == 'buffer.json':
+        open('progress.txt', 'w').write(str(epoch))
+        # save() saves the weights, model architecture, training configuration, and optimizer to a HDF5. 
+        # save_weights() only saves the weights to a HDF5. weights can be applied to another model architecture. 
         model.save('model.h5')
 
     JSON = dict()
     for i in range(data_size):
         JSON[np.array2string(X[i], separator = ", ", max_line_width = 1_000)] = float(Y[i])
-    json.dump(JSON, open(fstream, "w"), indent = 4)
+    json.dump(JSON, open(fstream, 'w'), indent = 4)
 
 table = [[1, 0, 0, 0, 0], [0, 1, 0, 0, 0], [0, 0, 1, 0, 0], [0, 0, 0, 1, 0], [0, 0, 0, 0, 1]]
 state = Tumbler([[1, 2, 3, 4, 5], 
@@ -97,6 +98,7 @@ Y = np.zeros([data_size], dtype = np.float32)
 state.move(table[4])
 value = np.array([0, 0, 0, 0, 1])
 
+#Import('buffer.json')
 #Synthesize()
 #Export('data.json')
 Import('data.json')
@@ -119,35 +121,34 @@ model.fit(X, Y, batch_size = 64, epochs = 100, verbose = 0)
 print("start program")
 for epoch in range(epoch, 1_000):
     print("epoch: ", epoch)
-    f = open('text.txt', "w")
-    f.write(str(epoch))
-    f.close()
+    open('progress.txt', 'w').write(str(epoch))
     model.save('model.h5')
+    Export('buffer.json')
 
     accuracy = 0
-    for what in range(0, data_size, 100):
-        for i in range(what, what + 100):
-            # simulate environment
-            reward = state.reward
-            action = table[value.argmax() if random.randrange(0, 100) < 95 else random.randrange(0, 5)]
-            X[i] = state.scrub(action)
-            state.move(action)
+    for i in range(data_size):
+        # simulate environment
+        reward = state.reward
+        action = table[value.argmax() if random.randrange(0, 100) < 95 else random.randrange(0, 5)]
+        X[i] = state.scrub(action)
+        state.move(action)
 
-            # replay buffer
-            if state.reward == 1_000:
-                Y[i] = reward + discount * 1_000
-                for temp in range(50):
-                    state.move(table[random.randrange(0, 5)])
+        # replay buffer
+        if state.reward == 1_000:
+            Y[i] = reward + discount * 1_000
+            for temp in range(50):
+                state.move(table[random.randrange(0, 5)])
 
-                value = model.predict(state.scrub_all(), verbose = 0)
-                accuracy += 1
-            else:
-                value = model.predict(state.scrub_all(), verbose = 0)
-                Y[i] = reward + discount * np.amax(value)
+            value = model.predict(state.scrub_all(), verbose = 0)
+            accuracy += 1
+        else:
+            value = model.predict(state.scrub_all(), verbose = 0)
+            Y[i] = reward + discount * np.amax(value)
 
         # train model
-        Qnew = keras.models.clone_model(model)
-        Qnew.compile(optimizer = 'adam', loss = 'mse')
-        print("loss:", "x" * min(100, int(Qnew.fit(X, Y, batch_size = 64, epochs = 100, verbose = 0).history['loss'][-1] // 4)))
-        model.set_weights(0.9 * np.array(model.get_weights(), dtype = object) + 0.1 * np.array(Qnew.get_weights(), dtype = object))
+        if not (i + 1) % 100:
+            Qnew = keras.models.clone_model(model)
+            Qnew.compile(optimizer = 'adam', loss = 'mse')
+            print("loss:", "x" * min(100, int(Qnew.fit(X, Y, batch_size = 64, epochs = 100, verbose = 0).history['loss'][-1] // 4)))
+            model.set_weights(0.9 * np.array(model.get_weights(), dtype = object) + 0.1 * np.array(Qnew.get_weights(), dtype = object))
     print("accuracy: ", accuracy * 5)
